56a375b22e677bfe3c05d1702d4785a28ca802e0
Florin Sorocianu <florin.sorocianu@iquestgroup.com>
2019-11-22 12:12:00 +0200

    Added tokenaquirer

--- CloudFileDownloader/CloudFileDownloader/Program.cs
+++ CloudFileDownloader/CloudFileDownloader/Program.cs
@@ -1,15 +1,22 @@
-﻿using System;
+﻿using OneDriveBrowser;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace CloudFileDownloader
 {
     class Program
     {
+
+        //[STAThread]
         static void Main(string[] args)
         {
+            MainWindow window = new  MainWindow();
+            window.InitializeComponent();
+
         }
     }
 }
